3400967928729b276c82a0b6ed4180402db56d5b
kate0427 <114397454+kate0427@users.noreply.github.com>
2022-12-17 16:19:18 +0100

    added axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  function showWeather(response) {
+    let temp = response.data.main.temp;
+    alert(`The temperature in Kyiv is ${temp}`);
+  }
+
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=kyiv&appid=e80f735c22f9cc78cdfe65b74bebba0a&units=metric`;
+  axios.get(apiURL).then(showWeather);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
